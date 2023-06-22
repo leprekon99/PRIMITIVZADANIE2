20b7d449aed08a128f2f633c8615e115527115a3
neto test <babenko.eliz@yandex.ru>
2023-06-22 13:04:13 +0300

    PRIMITIVZADANIE1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int AccountBalance = 100; // сумма счета
+        int BalanceReplenishment = 1100; // пополнение баланса
+        int BonusRubles = 100; // бонусные рубли за 1 рубль пополнения FinalScore итоговый счет numberOfBonusRubles
+
+        int FinalScore;
+        if (BalanceReplenishment > 1000) {
+            FinalScore = (BalanceReplenishment / BonusRubles) + AccountBalance + BalanceReplenishment;
+        } else {
+            FinalScore = (AccountBalance + BalanceReplenishment);
+        }
+
+        int numberOfBonusRubles;
+        if (BalanceReplenishment > 1000) {
+            numberOfBonusRubles = (BalanceReplenishment / BonusRubles);
+        } else {
+            numberOfBonusRubles = (0);
+        }
+
+        System.out.println(" Итоговый счет составляет " + FinalScore);
+        System.out.println(" Итого бонусов к зачислению " + numberOfBonusRubles);
+
+    }
+}
